87ba715f5190cc3912c97d94598a9ec0c0eeec01
Otavio Santana <otaviopolianasantana@gmail.com>
2024-11-11 15:20:35 +0000

    feat: update structure to movie repository
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/expert/os/demos/movies/domain/MovieRepository.java
+++ src/main/java/expert/os/demos/movies/domain/MovieRepository.java
@@ -3,10 +3,7 @@ package expert.os.demos.movies.domain;
 import jakarta.data.repository.BasicRepository;
 import jakarta.data.repository.Repository;
 
-import java.util.List;
-
 @Repository
 public interface MovieRepository extends BasicRepository<Movie, String> {
-    List<Movie> findByTitle(String title);
-    List<Movie> findByGenre(Genre genre);
+
 }
