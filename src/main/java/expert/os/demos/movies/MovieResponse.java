1f25438debd65edb4811f5cd0924fbe17b3466e3
Otavio Santana <otaviopolianasantana@gmail.com>
2024-11-11 15:01:50 +0000

    feat: include map structure
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/main/java/expert/os/demos/movies/MovieResponse.java
@@ -0,0 +1,30 @@
+package expert.os.demos.movies;
+
+import jakarta.json.bind.annotation.JsonbVisibility;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import java.util.List;
+
+
+@Schema(description = "Movie representation for API responses")
+@JsonbVisibility(FieldAccessStrategy.class)
+public class MovieResponse {
+
+    @Schema(description = "Unique identifier of the movie", example = "123e4567-e89b-12d3-a456-426614174000")
+    private String id;
+
+    @Schema(description = "Title of the movie", example = "Inception")
+    private String title;
+
+    @Schema(description = "Genre of the movie", example = "SCIENCE_FICTION")
+    private String genre;
+
+    @Schema(description = "Year the movie was released", example = "2010")
+    private int releaseYear;
+
+    @Schema(description = "Director of the movie", example = "Christopher Nolan")
+    private String director;
+
+    @Schema(description = "List of actors in the movie", example = "[\"Leonardo DiCaprio\", \"Joseph Gordon-Levitt\"]")
+    private List<String> actors;
+}
---
+++ src/main/java/expert/os/demos/movies/MovieService.java
@@ -0,0 +1,9 @@
+package expert.os.demos.movies;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class MovieService {
+
+    private final
+}
